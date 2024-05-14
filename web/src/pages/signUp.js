ccf04a82afa1186963b79487fd26bb2c9ec8982e
Sangamesh-RK <sangamesh99998@gmail.com>
2024-05-14 13:28:14 +0530

    Add Frontend for my api

---
+++ web/src/pages/signUp.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
+
+import UserForm from '../components/UserForm';
+import styled from 'styled-components';
+import Button from '../components/Button';
+
+const Wrapper = styled.div`
+  border: 1px solid #f5f4f0;
+  max-width: 500px;
+  padding: 1em;
+  margin: 0 auto;
+`;
+const Form = styled.form`
+  label,
+  input {
+    display: block;
+    line-height: 2em;
+  }
+  input {
+    width: 100%;
+    margin-bottom: 1em;
+  }
+`;
+const SIGNUP_USER = gql`
+  mutation signUp($email: String!, $username: String!, $password: String!) {
+    signUp(email: $email, username: $username, password: $password)
+  }
+`;
+
+// include the props passed to the component for later use
+const SignUp = props => {
+  useEffect(() => {
+    // update the document title
+    document.title = 'Sign Up — Notedly';
+  });
+  const client = useApolloClient();
+  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+    onCompleted: data => {
+      // store the token
+      localStorage.setItem('token', data.signUp);
+      // update the local cache
+      client.writeData({ data: { isLoggedIn: true } });
+      // redirect the user to the homepage
+      props.history.push('/');
+    }
+  });
+  return (
+    <React.Fragment>
+      <UserForm action={signUp} formType="signup" />
+      {/* if the data is loading, display a loading message*/}
+      {loading && <p>Loading...</p>}
+      {/* if there is an error, display a error message*/}
+      {error && <p>Error creating an account!</p>}
+    </React.Fragment>
+  );
+};
+export default SignUp;
